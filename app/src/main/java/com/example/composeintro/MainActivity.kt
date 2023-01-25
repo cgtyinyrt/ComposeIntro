a19af3114c0985644702fc29837c835b29a95986
Çağatay İnyurt <cagatayinyurt@outlook.com>
2023-01-25 17:52:46 +0300

    MainActivity.kt update.

--- app/src/main/java/com/example/composeintro/MainActivity.kt
+++ app/src/main/java/com/example/composeintro/MainActivity.kt
@@ -3,41 +3,76 @@ package com.example.composeintro
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.composeintro.ui.theme.ComposeIntroTheme
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ComposeIntroTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+            MainScreen()
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun MainScreen() {
+    // Column, Row, Box
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.LightGray),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        CustomText(text = "Hello Android!")
+        Spacer(modifier = Modifier.padding(5.dp))
+        CustomText(text = "Hello World!")
+        Spacer(modifier = Modifier.padding(5.dp))
+        CustomText(text = "Hello Kotlin!")
+        Spacer(modifier = Modifier.padding(5.dp))
+
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceEvenly
+        ) {
+            CustomText(text = "test 1")
+            CustomText(text = "test 2")
+        }
+    }
+}
+
+@Composable
+fun CustomText(text: String) {
+    Text(modifier = Modifier
+        .clickable {
+            println("clicked.")
+        }
+        .background(color = Color.Black)
+        .padding(top = 10.dp, start = 1.dp, end = 1.dp, bottom = 30.dp)
+        //.fillMaxSize(0.5f)
+        .width(150.dp),
+        text = text,
+        color = Color.White,
+        fontSize = 22.sp,
+        fontWeight = FontWeight.Bold,
+        textAlign = TextAlign.Center
+    )
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    ComposeIntroTheme {
-        Greeting("Android")
-    }
+    MainScreen()
 }
